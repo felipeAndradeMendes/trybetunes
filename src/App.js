fe5d5556148e762bc4ddf9f6e64a084a934f3b22
Felipe Mendes <felipemadruga@gmail.com>
2023-02-03 15:27:30 -0300

    Corrige erros de lint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -11,17 +11,17 @@ import Search from './pages/Search';
 class App extends React.Component {
   render() {
     return (
-    <div>
-      <Switch>
-        <Route path="/search" component={ Search } />
-        <Route path="/album/:id" component={ Album } />
-        <Route path="/favorites" component={ Favorites } />
-        <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route path="/profile" component={ Profile } />
-        <Route exact path="/" component={ Login }/>
-        <Route path="*" component={ NotFound } />
-      </Switch>
-    </div>
+      <div>
+        <Switch>
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/profile" component={ Profile } />
+          <Route exact path="/" component={ Login } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </div>
     );
   }
 }
