2cad3a91a2a849e110d5f91e6420696044fc0b66
Felipe Mendes <felipemadruga@gmail.com>
2023-02-03 17:38:31 -0300

    Corrige erros de lint

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
-import Loading from './pages/Loading';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
-import { createUser } from './services/userAPI';
 
 class App extends React.Component {
-
-  
-
   render() {
     // const {
     //   btnEntrar,
@@ -28,19 +23,11 @@ class App extends React.Component {
           <Route path="/favorites" component={ Favorites } />
           <Route path="/profile/edit" component={ ProfileEdit } />
           <Route path="/profile" component={ Profile } />
-          <Route exact path="/" component={ Login } /> 
+          <Route exact path="/" component={ Login } />
           <Route path="*" component={ NotFound } />
-
-            // <Login 
-              // disable={ btnEntrar }
-              // handleChange={ this.handleChange }
-              // handleClick={ this.createUserFunc }
-            // /> } 
-
-          />
+          {' '}
+          <Login />
         </Switch>
-
-
       </div>
     );
   }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { createUser } from '../services/userAPI';
 import Loading from './Loading';
 
 class Login extends React.Component {
-
   state = {
     btnEntrar: true,
     loginName: '',
     loading: false,
   };
 
-  handleChange = ({ target: { value, } }) => {
+  handleChange = ({ target: { value } }) => {
     const { loginName } = this.state;
     this.setState({
       loginName: value,
-      btnEntrar: loginName.length > 1 ? false : true,
+      btnEntrar: (loginName.length <= 1),
     });
-  }
+  };
 
   createUserFunc = async () => {
-    const { loginName, loading } = this.state;
+    const { loginName } = this.state;
     const { history } = this.props;
     // console.log('HISTORY', history)
     // console.log('INICIOU A FUNÇÃO')
@@ -29,22 +29,21 @@ class Login extends React.Component {
     });
     // console.log('CHAMOU O SETsTATE')
 
-    await createUser({name: loginName});
+    await createUser({ name: loginName });
     // console.log('ACABOU A FUNÇÃO ASYNC')
 
     this.setState({
       loading: false,
-    }, () => { history.push('/search')});
-  }
+    }, () => { history.push('/search'); });
+  };
 
-  render() { 
-    
+  render() {
     const {
       btnEntrar,
       loading,
     } = this.state;
-    
-    if (loading) return <Loading />
+
+    if (loading) return <Loading />;
     return (
       <div data-testid="page-login">
         <form>
@@ -54,11 +53,11 @@ class Login extends React.Component {
             type="text"
             onChange={ this.handleChange }
           />
-          <button 
+          <button
             type="button"
             data-testid="login-submit-button"
             disabled={ btnEntrar }
-            onClick={ this.createUserFunc }         
+            onClick={ this.createUserFunc }
           >
             Entrar
           </button>
@@ -67,5 +66,11 @@ class Login extends React.Component {
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
 
 export default Login;
