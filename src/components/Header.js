b996151a0495cbbd7eff6bfcec575e00c881cf2e
Felipe Mendes <felipemadruga@gmail.com>
2023-02-03 18:39:04 -0300

    Corrige erros de lint

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,35 +6,31 @@ class Header extends React.Component {
   state = {
     loginHeader: '',
     loading: true,
+  };
+
+  componentDidMount() {
+    this.getUserFunc();
   }
 
-  
   getUserFunc = async () => {
-   
     const login = await getUser();
     this.setState({
       loginHeader: login.name,
       loading: false,
     });
-  }
+  };
 
-  componentDidMount() {
-   this.getUserFunc() 
-  }
-
-
-
-render() {
-  const { loginHeader, loading } = this.state;   
-  if (loading) return <Loading />;
+  render() {
+    const { loginHeader, loading } = this.state;
+    if (loading) return <Loading />;
 
     return (
       <header data-testid="header-component">
         HEADER
         <p
           data-testid="header-user-name"
-        > 
-          { loginHeader } 
+        >
+          { loginHeader }
         </p>
       </header>
     );
