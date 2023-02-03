80e00d41b2464e0edbe418ea7447721c2e696cd2
Felipe Mendes <felipemadruga@gmail.com>
2023-02-03 16:33:12 -0300

    Desenvolvendo requisito 02

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
+import Loading from './pages/Loading';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
+import { createUser } from './services/userAPI';
 
 class App extends React.Component {
+
+  state = {
+    btnEntrar: true,
+    loginName: '',
+    loading: false,
+  };
+
+  handleChange = ({ target: { value, } }) => {
+    // console.log(value)
+    const { loginName } = this.state;
+    this.setState({
+      loginName: value,
+      btnEntrar: loginName.length > 1 ? false : true,
+    });
+  }
+
+  createUserFunc = async () => {
+    const { loginName, loading } = this.state;
+    // console.log('INICIOU A FUNÇÃO')
+    this.setState({
+      loading: true,
+    });
+    // console.log('CHAMOU O SETsTATE')
+    await createUser({name: loginName});
+    // console.log('ACABOU A FUNÇÃO ASYNC')
+
+    this.setState({
+      loading: false,
+    });
+    // console.log('CLIQUEI')
+    <Redirect to="/search" />
+  }
+
   render() {
+    const {
+      btnEntrar,
+      loading,
+    } = this.state;
+
+    if (loading) return <Loading />
     return (
       <div>
         <Switch>
@@ -18,9 +59,19 @@ class App extends React.Component {
           <Route path="/favorites" component={ Favorites } />
           <Route path="/profile/edit" component={ ProfileEdit } />
           <Route path="/profile" component={ Profile } />
-          <Route exact path="/" component={ Login } />
+          <Route exact path="/"
+            render={ () => 
+            <Login 
+              disable={ btnEntrar }
+              handleChange={ this.handleChange }
+              handleClick={ this.createUserFunc }
+            /> } 
+
+          />
           <Route path="*" component={ NotFound } />
         </Switch>
+
+
       </div>
     );
   }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,9 +2,30 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
+    const {
+      disable,
+      handleChange,
+      handleClick,
+    } = this.props;
+
     return (
       <div data-testid="page-login">
-        TESTANDO LOGIN
+        <form>
+          Nome:
+          <input
+            data-testid="login-name-input"
+            type="text"
+            onChange={ handleChange }
+          />
+          <button 
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ disable }
+            onClick={ handleClick }         
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
