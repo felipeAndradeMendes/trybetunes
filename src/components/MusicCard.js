05b7d1d62bee42e239da1e0c7e01051aa2bc08b7
Felipe Mendes <felipemadruga@gmail.com>
2023-02-04 17:29:30 -0300

    Continua desenvolvimento requistio 08

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from '../pages/Loading';
 
 class MusicCard extends React.Component {
+
+  state = {
+    loading: false,
+  };
+
+  handleChange = ({ target: {id} }) => {
+    const {
+      musicList,
+    } = this.props;
+
+    const trackClicked = musicList.find((track) => {
+      return track.trackId === Number(id);
+    });
+
+    console.log(trackClicked)
+    this.setFavorite(trackClicked); 
+  }
+
+  setFavorite = async (trackId) => {
+    this.setState({
+      loading: true,
+    }, async () => {
+      await addSong(trackId)
+      this.setState({
+        loading: false,
+      })
+    })
+  }
+
   render() {
     const {
       musicList,
     } = this.props;
+
+
+    // if (this.state.loading) return <Loading />;
+
     return (
       <div>
         <p>MUSIC CARD</p>
@@ -27,6 +62,16 @@ class MusicCard extends React.Component {
                     <code>audio</code>
                     .
                   </audio>
+                  <label htmlFor={ music.trackId }>
+                    Favorita
+                    <input 
+                      type="checkbox"
+                      id={ music.trackId }
+                      data-testid={`checkbox-music-${music.trackId}`}
+                      onChange={ this.handleChange }
+                    />
+
+                  </label>
                 </li>
               )
             ))}
